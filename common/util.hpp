a1f1322382df00abb94ef7033fc750aef069c536
mikko <mikko.finell@gmail.com>
2018-08-13 19:35:38 +0300

    rename helper to util

---
+++ common/util.hpp
@@ -0,0 +1,52 @@
+#ifndef __UTIL__
+#define __UTIL__
+
+#define WINW 1200
+#define WINH 1000
+#define TILEW 128
+#define TILEH TILEW / 2
+#define COLS_PER_TILE 4
+#define ROWS_PER_TILE 4
+#define CELLW TILEW / COLS_PER_TILE
+#define CELLH TILEH / ROWS_PER_TILE
+#define SPRIW 128
+#define SPRIH 128
+#define HALFW (TILEW * 0.5f)
+#define HALFH (TILEH * 0.5f)
+#define DIRECTIONS 4
+#define EDITOR_VERSION 0
+
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <cmath>
+
+namespace util {
+template<int W = CELLW, int H = CELLH>
+inline sf::Vector2f to_pixel(const sf::Vector2f & v) {
+    float x = (v.x - v.y) * W * 0.5f;
+    float y = (v.x + v.y) * H * 0.5f;
+    return {x, y};
+}
+
+template<int W = CELLW, int H = CELLH>
+inline sf::Vector2f to_map(const sf::Vector2f & v) {
+    float x = v.x / W + v.y / H;
+    float y = v.y / H - v.x / W;
+    return {x, y};
+}
+
+template<int W = CELLW, int H = CELLH>
+inline sf::Vector2f to_grid(const sf::Vector2f & v) {
+    auto w = to_map<W, H>(v);
+    w.x = std::floor(w.x);
+    w.y = std::floor(w.y);
+    return w;
+}
+
+inline void center_window(sf::RenderWindow & window) {
+    window.setPosition(sf::Vector2i{1920/2-WINW/2, 1080/2-WINH/2});
+}
+
+} // util
+
+#endif
