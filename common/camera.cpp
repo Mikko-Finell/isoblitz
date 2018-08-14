13423ca4a62e7e5c9208b2c64213f786c8aaa3b3
mikko <mikko.finell@gmail.com>
2018-08-14 21:11:17 +0300

    create Camera class

---
+++ common/camera.cpp
@@ -0,0 +1,25 @@
+#include "camera.hpp"
+#include <cassert>
+#include <iostream>
+
+Camera::Camera(sf::RenderWindow & w) : window(w) {
+}
+
+void Camera::zoom(float factor) {
+    zoomfactor /= factor;
+    auto v = window.getView();
+    v.zoom(1.0f / factor);
+    window.setView(v);
+}
+
+void Camera::scroll(const sf::Vector2f & dt) {
+    auto view = window.getView();
+    view.move(zoomfactor * dt);
+    window.setView(view);
+}
+
+void Camera::focus_at(const sf::Vector2f & point) {
+    auto view = window.getView();
+    view.setCenter(point);
+    window.setView(view);
+}
---
+++ common/camera.hpp
@@ -0,0 +1,17 @@
+#ifndef __CAMERA__
+#define __CAMERA__
+
+#include <SFML/Graphics.hpp>
+
+class Camera {
+    sf::RenderWindow & window;
+    float zoomfactor = 1.0f;
+
+public:
+    Camera(sf::RenderWindow & w);
+    void zoom(float factor);
+    void scroll(const sf::Vector2f & dt);
+    void focus_at(const sf::Vector2f & point);
+};
+
+#endif
