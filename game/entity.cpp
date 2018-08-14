d78630dc026073f02cb1f01c23c03730cc127fda
mikko <mikko.finell@gmail.com>
2018-08-14 21:14:52 +0300

    cleanup

--- game/entity.cpp
+++ game/entity.cpp
@@ -8,11 +8,8 @@ void Entity::update(gfx::time_t dt) {
     animation.update(dt);
 }
 
-void Entity::set_cell(Cell * c) {
-    assert(c != nullptr);
+void Entity::set_cell(const cell_t & c) {
     cell = c;
-    auto pos = cell->get_position();
-    //pos.x += TILEW/2;
-    //pos.y += TILEH/2;
+    auto pos = cell;
     animation.sprite.set_position(pos);
 }
